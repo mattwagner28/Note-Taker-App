003b8243e2c59a72c2288b3c6862ccf2e2c06991
MattWagner28 <matthewwagner@outlook.com>
2023-04-10 14:56:54 -0400

    after tutor session

--- Develop/server.js
+++ Develop/server.js
@@ -5,6 +5,7 @@ const util = require("util");
 
 // Helper method for generating unique ids
 const uuid = require('./helpers/uuid');
+const storage = require("./db/storage");
 
 const PORT = 3001;
 
@@ -70,6 +71,17 @@ app.post("/api/notes", (req, res) => {
     res.error("Error in adding note");
   }
 });
+
+
+
+
+
+app.delete("/api/notes/:id", (req, res) => {
+
+storage
+.removeNote(req.params.id).then(() => res.json({ok:true})).catch((err)=> res.status(500).json(err))
+
+})
 
 
 // listen() method is responsible for listening for incoming connections on the specified port
